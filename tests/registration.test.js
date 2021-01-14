481d445c41bf6b85dcf13bd7307807875fa062b0
psylocube <s0571034@htw-berlin.de>
2021-01-14 18:35:37 +0100

    first get test

--- tests/registration.test.js
+++ tests/registration.test.js
@@ -6,3 +6,12 @@
 const { expect } = require("chai");
 var request = require("supertest");
 var app = require("../app");
+
+describe("GET /user/registration", function(done){
+    it("should return 200 response", function(done){
+        request(app)
+            .get("/user/registration")
+            .expect(200, done);
+    });  
+    
+});
