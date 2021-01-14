5f00212f2008f80e89c1c8caacecad47602c8d76
psylocube <s0571034@htw-berlin.de>
2021-01-14 17:04:31 +0100

    added logout page logic for auth/notauth users

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -4,3 +4,22 @@
  */
 
 const passport = require("passport");
+
+exports.logOut = (req, res) => {
+    passport.authenticate('jwt', { session: false, failureFlash: false}, 
+    (err,user) => {
+      if (!user) { 
+        res.redirect('/user/login');
+      }
+  
+      if(user){
+        var token = null;
+        if (req && req.cookies && req.cookies['jwt']){
+          token = req.cookies['jwt'];
+          res.cookie('jwt', token, { httpOnly: true, secure: true, maxAge: '0' });
+          req.flash('success',"You have logged out");
+          res.redirect('/user/login');
+        }
+      } 
+    })(req,res)
+  }
