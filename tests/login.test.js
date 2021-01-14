f0ee5665c8832017bfbfd9ae752f4e6b00dc85a1
psylocube <s0571034@htw-berlin.de>
2021-01-14 18:48:19 +0100

    created file for login testing

---
+++ tests/login.test.js
@@ -0,0 +1,9 @@
+/**
+ * @author Nikita Shevchenko
+ * @description Testing the Login Page
+ */
+
+const { expect } = require("chai");
+var request = require("supertest");
+var app = require("../server");
+
