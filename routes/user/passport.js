32b9a5c82aa4c6629ecb500265cec4448011a91f
psylocube <s0571034@htw-berlin.de>
2021-01-14 16:43:19 +0100

    added jwt verifing strategy

--- routes/user/passport.js
+++ routes/user/passport.js
@@ -6,4 +6,27 @@ const passport = require("passport");
 const passportJWT = require('passport-jwt');
 const JWTStrategy = passportJWT.Strategy;
 
+var cookieExtractor = function(req) {
+    var token = null;
+    if (req && req.cookies)
+    {
+        token = req.cookies['jwt'];
+    }
+    return token;
+};
+
+//JWT Strategy
+passport.use(
+    new JWTStrategy({
+        jwtFromRequest: cookieExtractor, 
+        secretOrKey: process.env.JWT_SECRET_STRING, },
+        (jwtPayload, done) => {
+            if (Date.now() > jwtPayload.expires) {
+                return done(null, false, { message: "Please Log-In" });
+            }
+            return done(null, jwtPayload);
+        }
+    )
+);
+
 module.exports = passport;
