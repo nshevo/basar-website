66ca6479064bb1096a8445ceacd759f29282156d
psylocube <s0571034@htw-berlin.de>
2021-01-14 16:55:48 +0100

    dependencies

---
+++ controllers/registrationController.js
@@ -0,0 +1,8 @@
+/**
+ * @author Nikita Shevchenko
+ * @description Controller for the Registration Page
+ */
+
+var argon2 = require("argon2");
+const passport = require("passport");
+const User = require("../models/LoginModel");
