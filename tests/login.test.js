4387d8256c144985dd9322575cee39a7d02aa5e7
psylocube <s0571034@htw-berlin.de>
2021-01-14 18:50:04 +0100

    added some user data for testing

--- tests/login.test.js
+++ tests/login.test.js
@@ -7,3 +7,30 @@ const { expect } = require("chai");
 var request = require("supertest");
 var app = require("../server");
 
+//Credentials for testing the dashboard
+const userCredentials = {
+    email: "root@gmail.com", 
+    password: "123123"
+}
+
+const userCredentialsInvalid = {
+    email: "root@gmail.comm", 
+    password: "123123"
+}
+
+//Store jwt cookie for further testing of dashboard
+var jwtCookies;
+
+//Receive the jwt cookie
+before(function(done){
+    request(app)
+        .post("/user/login")
+        .send(userCredentials)
+        .end(function(err, response){
+            expect(response.statusCode).to.equal(302);
+            expect("Location", "/user/dashboard");
+            jwtCookies = response.headers["set-cookie"].pop().split(";")[0];
+            done();
+        });
+});
+
