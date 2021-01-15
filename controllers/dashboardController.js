24141fe7c40eb2332be366951c79a4003a46b008
psylocube <s0571034@htw-berlin.de>
2021-01-15 17:51:50 +0100

    controller for dashboard page with user logic

---
+++ controllers/dashboardController.js
@@ -0,0 +1,19 @@
+/**
+ * @author Nikita Shevchenko
+ * @description Controller for the Dashboard Page
+ */
+
+const passport = require("passport");
+
+//Profile/Dashabord Page - verify login
+exports.isNotLoggedIn = (req, res) => {
+    passport.authenticate('jwt', { session: false, failureFlash: false },
+        (err, user) => {
+            if (!user) {
+                res.redirect("/user/login");
+            }
+            if (user) {
+                res.render('user/dashboard', { title: "Dashboard", userFirstName: user.firstName, userLastName: user.lastName });
+            }
+        })(req, res)
+}
