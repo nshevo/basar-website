ba017e1cc9c5cb0d0f622f6ddc256adf3888a03a
KatOTB <50385698+KatOTB@users.noreply.github.com>
2021-03-04 17:52:53 +0100

    Css layout fix, small fix to index page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,14 +1,19 @@
+var Product = require('../models/product'); 
+var Cart = require('../models/cart'); 
 
 // renders a homepage
 const homepageGet = (req, res, next) =>  {
+    console.log('all products loaded'); 
 
-    //var r=res;
-    res.render('index',
-     { 
-        title: res.__("index.title"),
-        response: res
-     }
-    );
+    Product.find(function(err, products) {
+        if (err) {
+            console.log(err); 
+            next(err);
+        } else {
+         
+            res.render('allProducts', {title:res.__("allProducts.title"), response: res, products: products});
+        }
+    }); 
   }
 
  module.exports = {
