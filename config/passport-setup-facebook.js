3a80d68ca544ff42d377d32e4d6033d9c2ec29d6
paul0ss <pauloss228@gmail.com>
2021-03-23 00:38:29 +0100

    minor deployment changes

--- config/passport-setup-facebook.js
+++ config/passport-setup-facebook.js
@@ -16,7 +16,7 @@ passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     // callbackURL: "http://localhost:3000/auth/facebook/callback",
-    callbackURL: "https://basar-website.herokuapp.com/auth/facebook/callback",
+    callbackURL: process.env.HOST + "/auth/facebook/callback",
     profileFields: ['id', 'displayName', 'email', 'name']
   },
   function(accessToken, refreshToken, profile, done) {
--- config/passport-setup-google.js
+++ config/passport-setup-google.js
@@ -20,7 +20,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // callbackURL: "http://localhost:3000/auth/google/callback"
-    callbackURL: "https://basar-website.herokuapp.com/auth/google/callback"
+    callbackURL: process.env.HOST+"/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, email, done) {
       console.log("callback function");
