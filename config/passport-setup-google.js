6aeb53055b222dc4f8fc50eba3e51ff8057c7bc9
paul0ss <pauloss228@gmail.com>
2021-03-23 00:03:36 +0100

    Changed callbackURL of google

--- config/passport-setup-google.js
+++ config/passport-setup-google.js
@@ -19,7 +19,8 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    // callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://basar-website.herokuapp.com/auth/google"
   },
   function(accessToken, refreshToken, profile, email, done) {
       console.log("callback function");
