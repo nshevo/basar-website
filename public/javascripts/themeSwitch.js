81f617ca5f5b40f722a0c4ec31f8d482d9f69989
Andriy Lyubar <pauloss228@gmail.com>
2021-03-27 16:18:35 +0100

    fixed Light/Dark Mode

--- public/javascripts/themeSwitch.js
+++ public/javascripts/themeSwitch.js
@@ -1,40 +1,64 @@
 var script = document.createElement('script');
+var setTheme = localStorage.getItem('theme');
+var checkBox = document.getElementById("customSwitches");
+var text = document.getElementById("switchText");
 script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-function themeSwitch(){
-    var checkBox = document.getElementById("customSwitches");
-    var text = document.getElementById("switchText");
+function darkMode(){
+    console.log("Chechbox - checked (Dark)");
+    text.innerHTML = "Dark";
+    $("div[id='sticky-sidebar']").toggleClass("bg-light bg-dark");
+    $("div[id='sticky-sidebar']").toggleClass("text-dark text-white");
+    $("div[id='cards']").addClass("text-white bg-dark");
+    $("div[id='body']").addClass("bg-dark");
+    $("[id='categories_titel']").addClass("text-white");
+    $("a[class^='navbar-text']").each((i, ele) => {
+        console.log(ele);
+        $(ele).toggleClass('text-dark text-white');
+    });
+  }
 
-    // A $( document ).ready() block.
+  function lightMode(){
+    console.log("Chechbox - unchecked (Light)");
+    text.innerHTML = "Light";
+    $("div[id='sticky-sidebar']").toggleClass("bg-dark bg-light");
+    $("div[id='cards']").removeClass("text-white bg-dark");
+    $("div[id='body']").removeClass("bg-dark");
+    $("[id='categories_titel']").removeClass("text-white");
+    $("a[class^='navbar-text']").each((i, ele) => {
+        console.log(ele);
+        $(ele).toggleClass('text-white text-dark');
+    });
+  }
+
+window.onload = function(){
     $( document ).ready(function() {
+        let box = document.getElementById("customSwitches");
+        if(setTheme == null){
+            
+        }else if(setTheme == 'dark'){
+            box.checked = true;
+            darkMode();
+        }else if(setTheme == 'light'){
+            
+        }
+    })
+  };
+
+function themeSwitch(){
+    $( document ).ready(function() {
+        var checkBox = document.getElementById("customSwitches");
+        var text = document.getElementById("switchText");
         console.log( "ready!" );
+        
         if(checkBox.checked == true){
-            console.log("Chechbox - checked (Dark)");
-            text.innerHTML = "Dark";
-            $("[class$='-light']").each((i, ele) => {
-                console.log(ele);
-                $(ele).toggleClass('bg-light bg-dark')
-                $(ele).toggleClass('text-dark text-light')
-                $(ele).toggleClass('btn-dark btn-light')
-                $(ele).toggleClass('bg-light bg-dark')
-                $(ele).toggleClass('navbar-light navbar-dark')
-    
-            });
+            darkMode();
+            localStorage.setItem('theme', 'dark');
         }else{
-            console.log("Chechbox - unchecked (Light)");
-            text.innerHTML = "Light";
-            $("[class$='-dark']").each((i, ele) => {
-                console.log(ele);
-                $(ele).toggleClass('bg-dark bg-light')
-                $(ele).toggleClass('text-light text-dark')
-                $(ele).toggleClass('btn-dark btn-light')
-                $(ele).toggleClass('navbar-dark navbar-light')
-    
-            });
-            // toggle body class selector
-            $('body').toggleClass('dark light')
+            lightMode();
+            localStorage.setItem('theme', 'light');
         }
     });
 }
