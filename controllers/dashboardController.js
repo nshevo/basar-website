d101ffc0cd3884eafd4c4a3b9f7291cecbd031b2
psylocube <s0571034@htw-berlin.de>
2021-03-28 15:21:26 +0200

    minor cleaning

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -8,18 +8,6 @@ var loginController = require('./loginController');
 
 //Profile/Dashabord Page - verify login
 exports.isNotLoggedIn = (req, res) => {
-    // if(!req.user){
-    //     //user not logged in
-    //     res.redirect("/user/login");
-    // }else{
-    //     //user is logged in
-    //     console.log("We are here!");
-    //     console.log(req.user.firstName);
-    //     console.log(req.user.lastName);
-    //     res.render('user/dashboard.ejs', { title: "Dashboard", response: res, userFirstName: req.user.firstName, userLastName: req.user.lastName });
-    // }
-    // debug section
-    //console.log("We are here!"+loginController.loggedIn);
     if(!loginController.loggedIn){
         //user not logged in
         res.redirect("/user/login");
@@ -36,17 +24,7 @@ exports.isNotLoggedIn = (req, res) => {
                 }
             })(req, res)
         }else{
-            res.render('user/dashboard.ejs', { title: "Dashboard", response: res, userFirstName: req.user.firstName, userLastName: req.user.lastName });
+            res.render('user/dashboard', { title: "Dashboard", response: res, userFirstName: req.user.firstName, userLastName: req.user.lastName });
         }
     }
-
-    // passport.authenticate('jwt', { session: false, failureFlash: false },
-    //     (err, user) => {
-    //         if (!user) {
-    //             res.redirect("/user/login");
-    //         }
-    //         if (user) {
-    //             res.render('user/dashboard', { title: "Dashboard", response: res, userFirstName: user.firstName, userLastName: user.lastName });
-    //         }
-    //     })(req, res)
 }
